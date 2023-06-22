5667ac51b12670edda1e683b0a601861d9296aaa
Nur-Aiman <nur_aiman71099@yahoo.com>
2023-06-22 20:34:24 +0800

    Test

--- db/index.js
+++ db/index.js
@@ -1,28 +1,29 @@
 import pkg from 'pg'
 const { Pool } = pkg
+import 'dotenv/config'
 
 const db = new Pool({
-  host: 'localhost',
-  user: 'postgres',
-  password: '1234',
-  database: 'my-blog',
-  max: 20,
-  idleTimeoutMillis: 30000,
-  connectionTimeoutMillis: 2000,
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    max: 20,
+    idleTimeoutMillis: 30000,
+    connectionTimeoutMillis: 2000,
 })
 
-const query = async (text, params) => {
-  const start = Date.now()
-  const time = new Date()
-  const res = await db.query(text, params)
-  const duration = Date.now() - start
-  console.log('executed query', {
-    text,
-    start: time,
-    duration,
-    rows: res.rowCount,
-  })
-  return res
+const query = async(text, params) => {
+    const start = Date.now()
+    const time = new Date()
+    const res = await db.query(text, params)
+    const duration = Date.now() - start
+    console.log('executed query', {
+        text,
+        start: time,
+        duration,
+        rows: res.rowCount,
+    })
+    return res
 }
 
 export default query
